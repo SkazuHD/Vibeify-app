9d3bead951477e9263dc3f8c16c56a64dbdc8351
SkazuHD <voda0110@gmail.com>
2025-05-22 16:49:11 +0200

    POC Navigation

--- app/src/main/java/de/hsb/vibeify/MainActivity.kt
+++ app/src/main/java/de/hsb/vibeify/MainActivity.kt
@@ -1,24 +1,13 @@
 package de.hsb.vibeify
 
-import android.R.attr.onClick
-import android.content.Intent
+
 import android.os.Bundle
-import android.util.Log
-import androidx.compose.material3.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import de.hsb.vibeify.ui.theme.VibeifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,36 +16,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VibeifyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box {
+                        MainNavigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
 
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Button(
-            onClick = { },
-            modifier = modifier
-        ) {
-            Text("Click me")
-        }
-
-    }
-
-}
 
 
 
@@ -64,6 +32,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     VibeifyTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/de/hsb/vibeify/Navigation.kt
+++ app/src/main/java/de/hsb/vibeify/Navigation.kt
@@ -7,15 +7,25 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import de.hsb.vibeify.ui.Views.LoginView
+import de.hsb.vibeify.ui.Views.MainView
+import de.hsb.vibeify.ui.Views.Views
 
 @Composable
 fun MainNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "main") {
-        composable("main") { Greeting(
-            modifier = Modifier.padding(16.dp),
-            name = TODO()
-        ) }
+    NavHost(navController = navController, startDestination = Views.MainView.route) {
+        composable(Views.MainView.route) {
+           MainView(
+                navController = navController,
+                modifier = Modifier.padding(16.dp),
+            )
+            }
+        composable(Views.LoginView.route) {
+            LoginView(
+                modifier = Modifier.padding(16.dp),
+            )
+        }
     }
 }
